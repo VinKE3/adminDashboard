1a35bd870f25dd3a0aa88ad3a055b427dd5c83ef
Kevin Muñoz <harquin09@gmail.com>
2023-08-17 09:07:00 -0500

    refresh

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/(.*)",
-        destination: "/",
-      },
-    ];
-  },
   images: {
     domains: [
       "lh3.googleusercontent.com",

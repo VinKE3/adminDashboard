5d99a83c2cdad788fd7d3423babcf3f9f0f9d706
Kevin Muñoz <harquin09@gmail.com>
2023-08-17 08:46:03 -0500

    algo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "/",
+      },
+    ];
+  },
   images: {
     domains: [
       "lh3.googleusercontent.com",
